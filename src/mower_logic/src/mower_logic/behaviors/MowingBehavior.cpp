b1c2ad9f7d84819740e66047dc4fbea7aa32edb7
Cristian Nitescu <cristian@crowdsec.net>
2024-05-15 14:09:12 +0200

    avoid status race condition

--- src/mower_logic/src/mower_logic/behaviors/MowingBehavior.cpp
+++ src/mower_logic/src/mower_logic/behaviors/MowingBehavior.cpp
@@ -349,6 +349,7 @@ bool MowingBehavior::execute_mowing_plan() {
             moveBaseGoal.target_pose = path.path.poses.front();
             moveBaseGoal.controller = "FTCPlanner";
             mbfClient->sendGoal(moveBaseGoal);
+            sleep(1);
             actionlib::SimpleClientGoalState current_status(actionlib::SimpleClientGoalState::PENDING);
             ros::Rate r(10);
 
@@ -468,6 +469,7 @@ bool MowingBehavior::execute_mowing_plan() {
 
             ROS_INFO_STREAM("MowingBehavior: (MOW) First point reached - Executing mow path with " << path.path.poses.size() << " poses");            
             mbfClientExePath->sendGoal(exePathGoal);
+            sleep(1);
             actionlib::SimpleClientGoalState current_status(actionlib::SimpleClientGoalState::PENDING);
             ros::Rate r(10);
 
