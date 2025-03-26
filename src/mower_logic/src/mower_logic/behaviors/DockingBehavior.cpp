cad022b32be7487d27acfc5f4ac120d5986d2040
Cristian Nitescu <nitescuc@hotmail.com>
2025-03-26 20:21:39 +0100

    non blocking Docking bahavior

--- src/mower_logic/src/mower_logic/behaviors/DockingBehavior.cpp
+++ src/mower_logic/src/mower_logic/behaviors/DockingBehavior.cpp
@@ -21,12 +21,107 @@ extern ros::ServiceClient dockingPointClient;
 extern actionlib::SimpleActionClient<mbf_msgs::MoveBaseAction> *mbfClient;
 extern actionlib::SimpleActionClient<mbf_msgs::ExePathAction> *mbfClientExePath;
 extern mower_msgs::Status getStatus();
+extern ros::ServiceClient pathProgressClient;
 
 extern void stopMoving();
 extern bool setGPS(bool enabled);
 extern bool setGPSRtkFloat(bool enabled);
+extern bool isEmergencyMode();
+extern void setLidarEnabled(bool enabled);
 
 DockingBehavior DockingBehavior::INSTANCE;
+
+int getDockingMowPathIndex()
+{
+    ftc_local_planner::PlannerGetProgress progressSrv;
+    int currentIndex = -1;
+    if(pathProgressClient.call(progressSrv)) {
+        currentIndex = progressSrv.response.index;
+    } else {
+        ROS_ERROR("MowingBehavior: getMowIndex() - Error getting progress from FTC planner");
+    }
+    return(currentIndex);
+}
+
+bool DockingBehavior::execute_goal(actionlib::SimpleActionClient<mbf_msgs::MoveBaseAction> *client, mbf_msgs::MoveBaseGoal goal) {
+    client->sendGoal(goal);
+
+    bool goalSuccess = false;
+    bool waitingForResult = true;
+
+    ros::Rate r(10);
+
+    // we can assume the last_state is current since we have a security timer
+    int old_index = -1;
+    ros::Time last_index_time = ros::Time::now();
+    while (waitingForResult) {
+
+        r.sleep();
+
+        const auto last_status = getStatus();
+        auto mbfState = client->getState();
+
+        if(aborted) {
+            ROS_INFO_STREAM("Docking aborted.");
+            client->cancelGoal();
+            stopMoving();
+            goalSuccess = false;
+            waitingForResult = false;
+        }
+
+        switch (mbfState.state_) {
+            case actionlib::SimpleClientGoalState::ACTIVE:
+            case actionlib::SimpleClientGoalState::PENDING:
+                // currently moving. Cancel as soon as we're in the station
+                if (last_status.v_charge > 5.0) {
+                    ROS_INFO_STREAM("Got a voltage of " << last_status.v_charge << " V. Cancelling docking.");
+                    client->cancelGoal();
+                    stopMoving();
+                    goalSuccess = true;
+                    waitingForResult = false;
+                } else {
+                    int index = getDockingMowPathIndex();
+                    if ((index != old_index) || !this->hasGoodGPS() || isEmergencyMode()) {
+                        if (!this->hasGoodGPS())
+                            ROS_WARN_STREAM_THROTTLE(10, "DockingBehavior: (FIRST POINT) - No GPS signal, waiting.");
+                        if (isEmergencyMode())
+                            ROS_WARN_STREAM_THROTTLE(10, "DockingBehavior: (FIRST POINT) - Emergency mode, waiting.");
+                        last_index_time = ros::Time::now();
+                        old_index = index;
+                    } else {
+                        if (((ros::Time::now() - last_index_time).toSec() > 30.0) && !(this->paused || this->requested_pause_flag) && this->hasGoodGPS()) {
+                            ROS_ERROR_STREAM("DockingBehavior: (FIRST POINT) - No progress for 30 seconds, stopping path execution.");
+                            client->cancelAllGoals();
+                            stopMoving();
+                            goalSuccess = false;
+                            waitingForResult = false;
+                        }
+                    }
+                    ROS_INFO_STREAM_THROTTLE(5, "DockingBehavior: Goal Progress: " << index);
+                }
+                break;
+            case actionlib::SimpleClientGoalState::SUCCEEDED:
+                // we stopped moving because the path has ended. check, if we have docked successfully
+                if (last_status.v_charge > 5.0) {
+                    ROS_INFO_STREAM("Docking stopped, because we reached end pose. Voltage was " << last_status.v_charge << " V.");
+                    client->cancelGoal();
+                    stopMoving();
+                } else {
+                    ROS_INFO_STREAM("DockingBehavior: Goal reached");
+                }
+                goalSuccess = true;
+                waitingForResult = false;
+                break;
+            default:
+                ROS_WARN_STREAM("Some error during path execution. Docking failed. status value was: "
+                                        << mbfState.state_);
+                waitingForResult = false;
+                stopMoving();
+                break;
+        }
+    }
+    return goalSuccess;
+}
 
 bool DockingBehavior::approach_docking_point() {
     ROS_INFO_STREAM("Calculating approach path");
@@ -43,8 +138,7 @@ bool DockingBehavior::approach_docking_point() {
         mbf_msgs::MoveBaseGoal moveBaseGoal;
         moveBaseGoal.target_pose = fix_point;
         moveBaseGoal.controller = "FTCPlanner";
-        auto result = mbfClient->sendGoalAndWait(moveBaseGoal);
-        if (result.state_ != result.SUCCEEDED) {
+        if (!execute_goal(mbfClient, moveBaseGoal)) {
             ROS_ERROR_STREAM("Error reaching fix point");
             return false;
         }
@@ -82,8 +176,7 @@ bool DockingBehavior::approach_docking_point() {
         mbf_msgs::MoveBaseGoal moveBaseGoal;
         moveBaseGoal.target_pose = docking_approach_point;
         moveBaseGoal.controller = "FTCPlanner";
-        auto result = mbfClient->sendGoalAndWait(moveBaseGoal);
-        if (result.state_ != result.SUCCEEDED) {
+        if (!execute_goal(mbfClient, moveBaseGoal)) {
             return false;
         }
     }
@@ -219,10 +312,14 @@ Behavior *DockingBehavior::execute() {
         return &IdleBehavior::INSTANCE;
     }
 
+    setGPS(true);
     while(!isGPSGood){
         ROS_WARN_STREAM("Waiting for good GPS");
         ros::Duration(1.0).sleep();
     }
+    setLidarEnabled(true);
+    // give lidar a chance to start and fix
+    ros::Duration(1.0).sleep();
 
     bool approachSuccess = approach_docking_point();
 
@@ -250,6 +347,8 @@ Behavior *DockingBehavior::execute() {
       return &PerimeterSearchBehavior::INSTANCE;
     
     bool docked = dock_straight();
+
+    setLidarEnabled(false);
 
     if (!docked) {
         ROS_ERROR("Error during docking.");
